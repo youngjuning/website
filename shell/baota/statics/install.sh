ed4801c36e94694715d3fe1492cfc74462c30cbc
youngjuning <youngjuning@users.noreply.github.com>
2021-04-12 07:08:08 +0000

    deploy: 55654ee6b97091e0a1a70968ff0c35f65aca4d92

--- shell/baota/statics/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -tid --name baota -p 80:80 -p 443:443 -p 8888:8888 -p 888:888 -p 20:20 -p 21:21 --privileged=true --restart always -v baota_www:/www -v ~/wwwroot:/www/wwwroot pch18/baota
